69a50c2c9d5d9376fb35a69be940500f2a7f4802
HP <HP@DESKTOP-0QA7G3N>
2025-03-31 13:53:05 +0700

    first commit

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/JSP_SC";
     private static final String USER = "root"; // Thay bằng username của bạn
-    private static final String PASSWORD = "123456"; // Thay bằng password của bạn
+    private static final String PASSWORD = "quockhanh"; // Thay bằng password của bạn
 
     public static Connection getConnection() {
         try {
